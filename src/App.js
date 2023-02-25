ab69d6c8ed62b5507b6bee515f5e8e905c4c5fb4
saim <thesamirfan@gmail.com>
2023-02-25 12:41:37 +0530

    Update Style

--- src/App.js
+++ src/App.js
@@ -1,54 +1,46 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { Component } from 'react'
+// import Todo from "./Todo";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-    return (
-        <div className="App">
-            {/* <h1>Hello React JS, Welcome!!</h1> */}
-            <Navbar bg="light" expand="lg">
-                <Container fluid>
-                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">Link</Nav.Link>
-                            <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action4">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action5">
-                                    Something else here
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#" disabled>
-                                Link
-                            </Nav.Link>
-                        </Nav>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
+
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
+
+
+// function App() {
+//     return(
+//         <>
+//             <div>
+//                 <h1>TODO List</h1>
+//             </div>
+//         </>
+//     )
+// }
+
+class App extends Component {
+    state = {
+        items: [],
+        text: ""
+    }
+    render() {
+        return (
+            <>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm-6 mx-auto text-white">
+                            <h2>TODO List</h2>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// function Todo(props) {
+//     return <>
+//         <p>This is todo.</p>
+//     </>
+// }
+
+// export default Todo;
