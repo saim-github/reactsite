c369b3fde79463d79f7e78cc26ad88a754a7ea39
saim <thesamirfan@gmail.com>
2023-03-07 13:38:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react'
-// import Todo from "./Todo";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div>App</div>
-//     )
-//   }
-// }
-
-
-// function App() {
-//     return(
-//         <>
-//             <div>
-//                 <h1>TODO List</h1>
-//             </div>
-//         </>
-//     )
-// }
-
 class App extends Component {
-    state = {
-        items: [],
-        text: ""
-    }
     render() {
         return (
             <>
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-sm-6 mx-auto text-white">
+                        <div className="col-sm-6 mx-auto">
                             <h2>TODO List</h2>
                         </div>
                     </div>
--- src/Todo.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-// function Todo(props) {
-//     return <>
-//         <p>This is todo.</p>
-//     </>
-// }
-
-// export default Todo;
